cf6dc7ef8b58fbaed6deb99e2a35afa70af6dbcc
venkat ravilla <venkatravilla333@gmail.com>
2026-04-08 09:21:09 +0530

    objects completed

--- 6.Objects/index.js
+++ 6.Objects/index.js
@@ -139,15 +139,15 @@
 
 //How to loop object properties
 
-var person1 = {
-  name: 'sachin',
-  age: 45,
-  city: 'mumbai',
-  play: function () {
-     console.log('play')
-  }
-}
-console.log(person1)
+// var person1 = {
+//   name: 'sachin',
+//   age: 45,
+//   city: 'mumbai',
+//   play: function () {
+//      console.log('play')
+//   }
+// }
+// console.log(person1)
 
 //without using loop
 // console.log(person1.name)
@@ -170,6 +170,79 @@ console.log(person1)
 // var entreis = Object.entries(person1)
 // console.log(entreis)
 
+//copy
 
 
+// var x = 10
+
+// var y = x
+// y = 20
+// console.log(x)
+// console.log(y)
+
+
+// var originalObj = {
+//   name: 'sachin',
+//   address: { road: 101 },
+//   play: function () {
+//     console.log('play')
+//   }
+// }
+// import lodash from lodash
+
+// var lodash = require('lodash')
+// var copiedObj = originalObj // normal copy
+// var copiedObj = JSON.parse(JSON.stringify(originalObj)) // deep copy
+// var copiedObj = lodash.cloneDeep(originalObj) // deep copy
+// var copiedObj = Object.assign({}, originalObj) // shallow copy (ES-5)
+// var copiedObj = {...originalObj} // shallow copy (ES-6)
+
+
+// copiedObj.name = 'kohli'
+// copiedObj.address.road = 201
+// console.log(originalObj)
+// console.log(copiedObj)
+
+//Template string
+
+// var name = 'sachin'
+// var age = 45
+
+// console.log('Hi this is' + ' ' + name + ' ' + 'my age is' + ' ' + age  ) //ES-5
+// console.log(`Hi this is ${name} my age is ${age}`) //ES-6
+
+//Math object
+// var x = 10.4
+// var y = 15
+// console.log(Math.min(x, y))
+// console.log(Math.max(x, y))
+// console.log(Math.ceil(x))
+// console.log(Math.floor(x))
+// console.log(Math.round(x))
+// console.log(Math.random())
+
+//Date object
+
+// var now = new Date()
+// console.log(now)
+
+//Get methods
+// console.log(now.getFullYear())
+// console.log(now.getDate())
+// console.log(now.getMonth())
+// console.log(now.getDay())
+// console.log(now.getHours())
+// console.log(now.getMinutes())
+// console.log(now.getSeconds())
+
+//Set Methods
+
+// now.setFullYear(2040)
+// now.setMonth(6)
+// now.setHours(10)
+
+// console.log(now)
+
+// var date1 = new Date(2050, 10, 20, 10, 10, 10)
+// console.log(date1)
 
