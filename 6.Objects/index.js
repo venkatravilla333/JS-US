b7d7bdf0af298301351fe297aadd05414ba79a25
venkat ravilla <venkatravilla333@gmail.com>
2026-04-09 08:36:48 +0530

    string object

--- 6.Objects/index.js
+++ 6.Objects/index.js
@@ -246,3 +246,76 @@
 // var date1 = new Date(2050, 10, 20, 10, 10, 10)
 // console.log(date1)
 
+//string object
+
+var msg = 'hi How are you hi hi'
+// var msg = '333'
+console.log(msg)
+
+//How to find string length
+// console.log(msg.length)
+
+//How to remove white spaces
+
+// console.log(msg.trimStart().length)
+// console.log(msg.trimEnd().length)
+// console.log(msg.trim().length)
+
+//How to extract a part of string
+
+// console.log(msg.slice())
+// console.log(msg.slice(0))
+// console.log(msg.slice(0, 2))
+// console.log(msg.slice(0, 5))
+// console.log(msg.slice(-3))
+
+// console.log(msg.substring())
+// console.log(msg.substring(0))
+// console.log(msg.substring(0, 2))
+// console.log(msg.substring(0, 5))
+// console.log(msg.substring(-3))
+
+//How to convert string case
+
+// console.log(msg.toLowerCase())
+// console.log(msg.toUpperCase())
+
+//how to extract string char
+
+// console.log(msg.charAt(0))
+// console.log(msg.charCodeAt(0))
+
+//How to concat new with old string
+
+// console.log(msg.concat(' Sachin kohli'))
+
+//How to replace old string with new string
+
+// console.log(msg.replace('You', 'me'))
+
+//How to convert string into array
+
+// console.log(msg.split())
+// console.log(msg.split(''))
+// console.log(msg.split(' '))
+
+//How to pad or add new string to old string
+
+// console.log(msg.padStart(6, 'Hi'))
+// console.log(msg.padEnd(6, 2))
+
+//How to find specific
+
+// console.log(msg.indexOf('hi'))
+// console.log(msg.indexOf('bye'))
+// console.log(msg.lastIndexOf('hi'))
+// console.log(msg.lastIndexOf('bye'))
+// console.log(msg.includes('you'))
+// console.log(msg.includes('me'))
+// console.log(msg.startsWith('hi'))
+// console.log(msg.startsWith('bye'))
+// console.log(msg.endsWith('hi'))
+// console.log(msg.endsWith('bye'))
+
+
+
